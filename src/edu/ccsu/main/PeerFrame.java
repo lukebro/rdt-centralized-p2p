434baf22b2fec2d7d8e0a0a1fec50e72f74c589b
kvn-dly <kvn_dly@yahoo.com>
2015-04-27 18:34:34 -0400

    added window listener, no function

--- src/edu/ccsu/main/PeerFrame.java
+++ src/edu/ccsu/main/PeerFrame.java
@@ -1,6 +1,10 @@
 package edu.ccsu.main;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import edu.ccsu.gui.PeerPanel;
+
 import javax.swing.JFrame;
 
 public class PeerFrame {
@@ -10,11 +14,19 @@ public class PeerFrame {
 		JFrame frame = new JFrame ("Napster Client");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		frame.addWindowListener(new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent e){
+				
+			}
+		});
+		
 		PeerPanel panel = new PeerPanel();
 		frame.getContentPane().add(panel);
 		frame.pack();
 		frame.setVisible(true);
 		frame.setResizable(false);
 	}
+	
 
 }
